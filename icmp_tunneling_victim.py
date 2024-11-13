a99477c251b3963cd40b21033af4d7b7429dfdec
Kanskikuro <william.vo@live.no>
2024-11-13 12:37:55 +0100

    Create icmp_tunneling_victim.py

---
+++ icmp_tunneling_victim.py
@@ -0,0 +1,12 @@
+# victim_script.py
+from scapy.all import sniff, IP, ICMP
+
+def extract_covert_data(packet):
+    if packet.haslayer(ICMP):
+        icmp_payload = bytes(packet[ICMP].payload)
+        if icmp_payload:
+            print("Received covert data:", icmp_payload.decode(errors='ignore'))
+
+if __name__ == "__main__":
+    print("Sniffing for ICMP packets...")
+    sniff(filter="icmp", prn=extract_covert_data, store=0)
